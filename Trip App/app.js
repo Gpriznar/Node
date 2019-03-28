e75900f0ba2b21d552808707ef03fb3f97dc538f
Gregory Priznar <44954435+Gpriznar@users.noreply.github.com>
2019-03-28 14:58:12 -0500

    Updates to Trip App

--- Trip App/app.js
+++ Trip App/app.js
@@ -1,24 +1,85 @@
 const express = require('express')
 const mustacheExpress = require('mustache-express')
 const bodyParser = require('body-parser')
+const path = require('path')
+var session = require('express-session')
 const app = express()
+
+const VIEWS_PATH= path.join(__dirname, '/views');
+console.log(VIEWS_PATH)
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}))
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
-
 // tell express to use mustache templating engine
-app.engine('mustache',mustacheExpress())
+app.engine('mustache', mustacheExpress(VIEWS_PATH + '/partials', '.mustache'))
 // the pages are located in views directory
 app.set('views','./views')
 // extension will be .mustache
 app.set('view engine','mustache')
 
+//     Users      //
+
+let users = []
+let persistedUser = {}
+
+app.post('/register', (req,res) => {
+  let username = req.body.username
+  let password = req.body.password
+
+  let user = {username: username, password: password, userTrips: []}
+  users.push(user)
+  console.log(users)
+
+  res.render('home', {messageA: 'Thank you for registering! Please login below.'})
+})
+
+app.post('/login', (req,res) => {
+  let username = req.body.username
+  let password = req.body.password
+
+  let user = {username: username, password: password, userTrips: []}
+
+    persistedUser = users.find((user) => {
+    return user.username == username && user.password == password
+  })
+
+  if(persistedUser) {
+    if(req.session) {
+      req.session.username = persistedUser.username
+      res.redirect('/addtrip')
+    }
+  } else {
+    res.render('home', {message: 'The User Name or Password is incorrect. Please try again!'})
+  }
+})
+
+app.post('/logout', (req,res) => {
+  req.session.destroy(function(err) {
+    if(err) {
+      console.log(err)
+    } else {
+      res.redirect('/home')
+    }
+  })
+})
+
+app.get('/home', (req,res) => {
+  res.render('home')
+})
+
+//     Trips      //
+
 let trips = []
 
 app.get("/addtrip", (req,res) => {
-  res.render('addtrip',{trips: trips})
+  res.render('addtrip',{trips: persistedUser.userTrips})
 })
-
 
 app.post('/addtrip',(req,res) => {
   let title = req.body.tripTitle
@@ -27,10 +88,9 @@ app.post('/addtrip',(req,res) => {
   let dor = req.body.tripDor
   let id = guid()
 
-
-
 let tripslist = ({title: title, image: image, dod: dod, dor: dor, id: id})
-trips.push(tripslist)
+persistedUser.userTrips.push(tripslist)
+console.log(persistedUser)
 
 res.redirect('/addtrip')
 })
@@ -49,6 +109,8 @@ app.post('/deletetrip',(req,res) =>{
 
   res.render('addtrip',{trips:trips})
 })
+
+// Listen for server //
 
 app.listen(3000,() => {
 console.log("Server is running...")
